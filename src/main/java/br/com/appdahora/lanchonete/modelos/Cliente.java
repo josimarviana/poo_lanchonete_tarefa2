5fa9ef361f8b1e99789836b7f6880301cddd32db
Josimar Viana Silva <josimarviana@gmail.com>
2022-10-04 23:45:17 -0300

    comentando o properties

--- src/main/java/br/com/appdahora/lanchonete/modelos/Cliente.java
+++ src/main/java/br/com/appdahora/lanchonete/modelos/Cliente.java
@@ -3,7 +3,7 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Cliente extends Pessoa{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
