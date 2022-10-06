95d1ce060703e64038fa92dd113ac50a3453005d
Josimar Viana Silva <josimarviana@gmail.com>
2022-10-06 16:40:56 -0300

    xml no pom

--- src/main/java/br/com/appdahora/lanchonete/api/controller/ClienteController.java
+++ src/main/java/br/com/appdahora/lanchonete/api/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.appdahora.lanchonete.api.controller;
 import br.com.appdahora.lanchonete.domain.model.Cliente;
 import br.com.appdahora.lanchonete.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class ClienteController {
     private ClienteRepository clienteRepository;
     
     @GetMapping
+    //@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cliente> listar(){
         return clienteRepository.listar();
     }
