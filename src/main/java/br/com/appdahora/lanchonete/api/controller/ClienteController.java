6646ad305dee0160e512f8ba36f3d55f436240e5
Josimar Viana Silva <josimarviana@gmail.com>
2022-10-10 17:26:06 -0300

    responseentity

--- src/main/java/br/com/appdahora/lanchonete/api/controller/ClienteController.java
+++ src/main/java/br/com/appdahora/lanchonete/api/controller/ClienteController.java
@@ -4,11 +4,10 @@ import br.com.appdahora.lanchonete.api.model.ClientesXmlWrapper;
 import br.com.appdahora.lanchonete.domain.model.Cliente;
 import br.com.appdahora.lanchonete.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,9 +27,17 @@ public class ClienteController {
         return new ClientesXmlWrapper(clienteRepository.listar());
     }
     // /clientes/{clienteId}
+
+    //@ResponseStatus(HttpStatus.CREATED) // Altera o código de resposta HTTP
     @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente>  buscar(@PathVariable Long clienteId){
+        Cliente cliente =  clienteRepository.buscar(clienteId);
+       // return ResponseEntity.status(HttpStatus.OK).body(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+    /*
     public Cliente buscar(@PathVariable Long clienteId){
         return clienteRepository.buscar(clienteId);
-    }
+    }*/
 
 }
