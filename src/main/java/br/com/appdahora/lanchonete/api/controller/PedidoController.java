25221f0059bdf1e116f1c4399b5e04302545187b
Josimar Viana Silva <josimarviana@gmail.com>
2022-10-18 00:26:11 -0300

    tratando atualização pedido para não pagar os itens de Pedido

--- src/main/java/br/com/appdahora/lanchonete/api/controller/PedidoController.java
+++ src/main/java/br/com/appdahora/lanchonete/api/controller/PedidoController.java
@@ -65,7 +65,7 @@ public class PedidoController {
         try{
             Optional<Pedido>  pedidoAtual =  pedidoRepository.findById(pedidoId);
             if (pedidoAtual.isPresent()) { //verifica se está vazio
-                BeanUtils.copyProperties(pedido, pedidoAtual.get(), "id");
+                BeanUtils.copyProperties(pedido, pedidoAtual.get(), "id", "itemPedido");
                 Pedido pedidoSalvo = cadastroPedidoService.salvar(pedidoAtual.get());
                 return ResponseEntity.ok(pedidoSalvo);
             }
