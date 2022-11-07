35e344dc4f748cc71b77f5602cab4682fb6ca3fd
josimarviana <josimarviana@gmail.com>
2022-11-07 16:10:50 -0300

    inserindo bd h2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,11 +13,11 @@ insert into forma_pagamento (descricao) values ('Cartão de débito');
 insert into forma_pagamento (descricao) values ('Dinheiro');
 insert into forma_pagamento (descricao) values ('PIX');
 
-insert ignore into cozinha (nome) values ('Quitandas');
-insert ignore into cozinha (nome) values ('Hamburguerias');
+insert into cozinha (nome) values ('Quitandas');
+insert into cozinha (nome) values ('Hamburguerias');
 
-insert into restaurante (nome, descricao, cozinha_id) values ("fatinha", "empadas x", 1);
-insert into restaurante (nome, descricao, cozinha_id) values ("galpao", "galpao do hamburguer", 2);
+insert into restaurante (nome, descricao, cozinha_id) values ('fatinha', 'empadas x', 1);
+insert into restaurante (nome, descricao, cozinha_id) values ('galpao', 'galpao do hamburguer', 2);
 
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (1, 4), (2, 1), (2, 2), (2, 3), (2, 4);
 
@@ -29,17 +29,17 @@ insert into produto (nome, descricao, preco, restaurante_id) values ('Empada Pim
 insert into produto (nome, descricao, preco, restaurante_id) values ('Sanduiche Ixe', 'Sanduiche Ixe', 20, 2);
 insert into produto (nome, descricao, preco, restaurante_id) values ('Sanduiche Promo', 'Sadcuiche Promo', 10, 2);
 
-insert into pedido (data_solicitacao, data_atualizacao, valor_total, cliente_id, restaurante_id ,status_pedido) values (utc_timestamp, utc_timestamp, 20.00, 1, 1, "PENDENTE");
-insert into pedido (data_solicitacao, data_atualizacao, valor_total, cliente_id, restaurante_id ,status_pedido) values (utc_timestamp, utc_timestamp, 20.00, 2, 2, "PENDENTE");
+insert into pedido (data_solicitacao, data_atualizacao, valor_total, cliente_id, restaurante_id ,status_pedido) values ('2020-11-11', '2020-11-11', 20.00, 1, 1, 'PENDENTE');
+insert into pedido (data_solicitacao, data_atualizacao, valor_total, cliente_id, restaurante_id ,status_pedido) values ('2020-11-11', '2020-11-11', 20.00, 2, 2, 'PENDENTE');
 
 insert into item_pedido (produto_id, quantidade, preco_unitario, preco_total, pedido_id) values (1, 1, 20, 20, 1);
 insert into item_pedido (produto_id, quantidade, preco_unitario, preco_total, pedido_id) values (2, 1, 10, 10, 2);
 insert into item_pedido (produto_id, quantidade, preco_unitario, preco_total, pedido_id) values (3, 1, 30, 30, 2);
 
-insert into ocorrencia_entrega (pedido_id, descricao, data_registro) values (1, "Tentativa sem sucesso", utc_timestamp);
---insert into ocorrencia_entrega (pedido_id, descricao, data_registro) values (2, "Entrega realizada", utc_timestamp);
+insert into ocorrencia_entrega (pedido_id, descricao, data_registro) values (1, 'Tentativa sem sucesso', '2020-11-11');
+--insert into ocorrencia_entrega (pedido_id, descricao, data_registro) values (2, 'Entrega realizada', '2020-11-11');
 
-update cliente set logradouro="x", numero="71", bairro="centro", cidade_id=1, cep="38600000" where id = 1;
-update cliente set logradouro="y", numero="72", bairro="centro", cidade_id=2, cep="38600000" where id = 2;
-update cliente set logradouro="z", numero="73", bairro="centro", cidade_id=3, cep="38600000" where id = 3;
+update cliente set logradouro='x', numero='71', bairro='centro', cidade_id=1, cep='38600000' where id = 1;
+update cliente set logradouro='y', numero='72', bairro='centro', cidade_id=2, cep='38600000' where id = 2;
+update cliente set logradouro='z', numero='73', bairro='centro', cidade_id=3, cep='38600000' where id = 3;
 
