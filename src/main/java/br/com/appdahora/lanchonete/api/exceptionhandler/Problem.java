8e8cd951f97cb85ca8fa94a4bc09e6c0452b7fe8
josimarviana <josimarviana@gmail.com>
2022-11-16 14:33:59 -0300

    tratando outros erros

--- src/main/java/br/com/appdahora/lanchonete/api/exceptionhandler/Problem.java
+++ src/main/java/br/com/appdahora/lanchonete/api/exceptionhandler/Problem.java
@@ -5,15 +5,19 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL) //não insere atributos nulos no json
 public class Problem {
     private Integer status;
+    private LocalDateTime timestamp;
     private String type;
     private String title;
     private String detail;
+    private String userMessage;
 
 
 //    private OffsetDateTime dataHora;
